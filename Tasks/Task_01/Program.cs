962e9daafed4aa67bc89c05e32dfb3ab5803753b
Kaciade <valerypolyakov60@gmail.com>
2023-02-07 03:47:35 +0400

    Задание 1 выполнено

---
+++ Tasks/Task_01/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] array = new int [8];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++) array[i] = random.Next(0, 2);
+for (int j = 0; j < array.Length; j++) Console.Write($"| {array[j]} ");

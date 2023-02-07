f5da9f56b7efbe26b2e0898d298ec4252999ce21
Kaciade <valerypolyakov60@gmail.com>
2023-02-07 14:19:02 +0400

    Задание 4 выполнено

--- Tasks/Task_04/Program.cs
+++ Tasks/Task_04/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+Console.Write("Введите количество элементов массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите левую часть диапазона массива: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите правую часть диапазона массива: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int [length];
+Random random = new Random();
+int oddSum = 0;
+
+for (int i = 0; i < array.Length; i++) array[i] = random.Next(min, max);
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]}\t");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2 == 0) oddSum = oddSum + array[i];
+}
+
+Console.WriteLine($"\nСумма всех нечётных элементов массива равна {oddSum}");

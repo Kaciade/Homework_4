50903b1ac6efb57b89368bb27872356c100d89c1
Kaciade <valerypolyakov60@gmail.com>
2023-02-08 17:54:18 +0400

    Задание 6

---
+++ Tasks/Task_06/Program.cs
@@ -0,0 +1,17 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+Console.Write("Введите количество элементов массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите левую часть диапазона массива: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите правую часть диапазона массива: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int [length];
+Random random = new Random();
+int j = array.Length;
+int stop = j/2;
+
+for (int i = 0; i < array.Length; i++) array[i] = random.Next(min, max);
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]}\t");
+Console.WriteLine();
